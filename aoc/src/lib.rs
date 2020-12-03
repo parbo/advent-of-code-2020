4a70925fbe9c792f3da85678e1d33fc4931d8c5c
Pär Bohrarper <bohrarper@spotify.com>
2020-12-03 11:54:26 +0100

    add graph stuff

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -12,11 +12,98 @@ use std::marker::PhantomData;
 use std::num::ParseIntError;
 use std::path::Path;
 
+#[macro_use]
+extern crate lazy_static;
+
+extern crate vecmath;
+
 pub use mod_exp::mod_exp;
 pub use modinverse::modinverse;
 pub use num::integer::*;
+pub use petgraph::graphmap::UnGraphMap;
 pub use serde_scan::from_str;
 pub use serde_scan::scan;
+
+pub type Point = self::vecmath::Vector2<i64>;
+pub type FPoint = self::vecmath::Vector2<f64>;
+pub type Vec3 = self::vecmath::Vector3<i64>;
+pub type FVec3 = self::vecmath::Vector3<f64>;
+pub type Vec4 = self::vecmath::Vector4<i64>;
+pub type FVec4 = self::vecmath::Vector4<f64>;
+pub type Mat4 = self::vecmath::Matrix4<i64>;
+pub type FMat4 = self::vecmath::Matrix4<f64>;
+
+pub use self::vecmath::mat4_id as mat_identity;
+pub use self::vecmath::mat4_transposed as mat_transpose;
+pub use self::vecmath::row_mat4_mul as mat_mul;
+pub use self::vecmath::row_mat4_transform as mat_transform;
+pub use self::vecmath::vec2_add as point_add;
+pub use self::vecmath::vec2_dot as point_dot;
+pub use self::vecmath::vec2_neg as point_neg;
+pub use self::vecmath::vec2_normalized as point_normalize;
+pub use self::vecmath::vec2_square_len as point_square_length;
+pub use self::vecmath::vec2_sub as point_sub;
+pub use self::vecmath::vec3_add as vec_add;
+pub use self::vecmath::vec3_cross as vec_cross;
+pub use self::vecmath::vec3_dot as vec_dot;
+pub use self::vecmath::vec3_neg as vec_neg;
+pub use self::vecmath::vec3_normalized as vec_normalize;
+pub use self::vecmath::vec3_scale as vec_mul;
+pub use self::vecmath::vec3_scale as point_mul;
+pub use self::vecmath::vec3_square_len as vec_square_length;
+pub use self::vecmath::vec3_sub as vec_sub;
+
+pub fn length(v: FVec3) -> f64 {
+    vec_square_length(v).sqrt()
+}
+
+pub fn cmul(v1: Vec3, v2: Vec3) -> Vec3 {
+    let [x1, y1, z1] = v1;
+    let [x2, y2, z2] = v2;
+    [x1 * x2, y1 * y2, z1 * z2]
+}
+
+pub static NORTH: Point = [0, -1];
+pub static UP: Point = NORTH;
+pub static NORTH_EAST: Point = [1, -1];
+pub static UP_RIGHT: Point = NORTH_EAST;
+pub static EAST: Point = [1, 0];
+pub static RIGHT: Point = EAST;
+pub static SOUTH_EAST: Point = [1, 1];
+pub static DOWN_RIGHT: Point = SOUTH_EAST;
+pub static SOUTH: Point = [0, 1];
+pub static DOWN: Point = SOUTH;
+pub static SOUTH_WEST: Point = [-1, 1];
+pub static DOWN_LEFT: Point = SOUTH_WEST;
+pub static WEST: Point = [-1, 0];
+pub static LEFT: Point = WEST;
+pub static NORTH_WEST: Point = [-1, -1];
+pub static UP_LEFT: Point = NORTH_WEST;
+
+lazy_static! {
+    pub static ref DIRECTIONS: Vec<Point> = { vec![NORTH, EAST, SOUTH, WEST] };
+    pub static ref DIRECTIONS_INCL_DIAGONALS: Vec<Point> = {
+        vec![
+            NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST,
+        ]
+    };
+    pub static ref DIRECTION_MAP: HashMap<&'static str, Point> = {
+        let mut map = HashMap::new();
+        map.insert("U", NORTH);
+        map.insert("D", SOUTH);
+        map.insert("R", EAST);
+        map.insert("L", WEST);
+        map.insert("N", NORTH);
+        map.insert("S", SOUTH);
+        map.insert("E", EAST);
+        map.insert("W", WEST);
+        map.insert("NW", NORTH_WEST);
+        map.insert("SW", SOUTH_WEST);
+        map.insert("NE", NORTH_WEST);
+        map.insert("SE", SOUTH_EAST);
+        map
+    };
+}
 
 #[derive(Debug)]
 pub enum ParseError {
@@ -69,6 +156,47 @@ pub fn parse_str_grid(lines: &[&str]) -> Vec<Vec<char>> {
 
 pub fn parse_str_grid_to<T>(lines: &[&str], f: fn(char) -> T) -> Vec<Vec<T>> {
     lines.iter().map(|x| x.chars().map(f).collect()).collect()
+}
+
+pub fn grid_to_graph<T>(
+    grid: &Vec<Vec<T>>,
+    is_node: fn(&Point, &T) -> bool,
+    get_edge: fn(&Point, &T, &Point, &T) -> Option<i64>,
+    directions: usize,
+) -> UnGraphMap<Point, i64> {
+    let directions: Vec<_> = match directions {
+        4 => DIRECTIONS.clone(),
+        8 => DIRECTIONS_INCL_DIAGONALS.clone(),
+        _ => panic!(),
+    };
+
+    let mut graph = UnGraphMap::new();
+    let ysize = grid.len();
+    let xsize = grid[0].len();
+
+    for y in 0..ysize {
+        for x in 0..xsize {
+            let p: Point = [x as i64, y as i64];
+            let c = &grid[y][x];
+            if is_node(&p, &c) {
+		let gp = graph.add_node(p);
+                for d in &directions {
+                    let np = point_add(p, *d);
+                    if np[0] >= 0 && np[0] < xsize as i64 && np[1] >= 0 && np[1] < ysize as i64 {
+                        let nc = &grid[np[1] as usize][np[0] as usize];
+                        if is_node(&np, &nc) {
+                            if let Some(e) = get_edge(&p, &c, &np, &nc) {
+				let gnp = graph.add_node(np);
+				graph.add_edge(gp, gnp, e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    graph
 }
 
 pub fn get_char(s: &str, ix: usize) -> Option<char> {
