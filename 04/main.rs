0cb4bb8d384d2cfcb045cc9ab06b5007313044ff
Pär Bohrarper <bohrarper@spotify.com>
2020-12-04 09:23:28 +0100

    fix bug and add tests

--- 04/main.rs
+++ 04/main.rs
@@ -61,7 +61,7 @@ fn is_valid_details(p: &HashMap<String, String>) -> bool {
                     || v == "hzl"
                     || v == "oth"
             }
-            "pid" => v.chars().filter(|c| c.is_numeric()).count() == 9,
+            "pid" => v.len() == 9 && v.chars().filter(|c| c.is_numeric()).count() == 9,
             "cid" => true,
             _ => false,
         })
@@ -103,4 +103,59 @@ fn main() {
         part2(&parsed)
     };
     println!("{}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part2() {
+        let invalid: Vec<_> = vec![
+            "eyr:1972 cid:100",
+            "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+            "",
+            "iyr:2019",
+            "hcl:#602927 eyr:1967 hgt:170cm",
+            "ecl:grn pid:012533040 byr:1946",
+            "",
+            "hcl:dab227 iyr:2012",
+            "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+            "",
+            "hgt:59cm ecl:zzz",
+            "eyr:2038 hcl:74454a iyr:2023",
+            "pid:3556412378 byr:2007",
+            "",
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093abcd154719",
+        ]
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect();
+
+        for passport in parse(&invalid) {
+            assert_eq!(is_valid_details(&passport), false, "error: {:?}", passport);
+        }
+
+        let valid: Vec<_> = vec![
+            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+            "hcl:#623a2f",
+            "",
+            "eyr:2029 ecl:blu cid:129 byr:1989",
+            "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+            "",
+            "hcl:#888785",
+            "hgt:164cm byr:2001 iyr:2015 cid:88",
+            "pid:545766238 ecl:hzl",
+            "eyr:2022",
+            "",
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+        ]
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect();
+
+        for passport in parse(&valid) {
+            assert_eq!(is_valid_details(&passport), true, "error: {:?}", passport);
+        }
+    }
 }
