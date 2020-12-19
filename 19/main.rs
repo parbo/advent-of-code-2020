09c837e9d30cc301c227f2e687ac5212397f8f07
Pär Bohrarper <par.bohrarper@gmail.com>
2020-12-19 10:26:16 +0100

    day 19 pt 1

--- 19/main.rs
+++ 19/main.rs
@@ -1,6 +1,7 @@
 use aoc::ParseError;
 use std::iter::*;
 use std::str::FromStr;
+use regex::Regex;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Rule {
@@ -53,10 +54,57 @@ impl FromStr for Rule {
 type Parsed = (Vec<Rule>, Vec<String>);
 type Answer = usize;
 
+fn expand_rule(rules: &[Rule], ix: usize) -> Vec<String> {
+    match rules[ix] {
+	Rule::Char(c) => vec![c.to_string()],
+	Rule::One(i) => expand_rule(rules, i),
+	Rule::OneOr(a, b) => {
+	    let mut v = vec!["(".to_string()];
+	    v.append(&mut expand_rule(rules, a));
+	    v.push("|".to_string());
+	    v.append(&mut expand_rule(rules, b));
+	    v.push(")".to_string());
+	    v
+	}
+	Rule::Two(a, b) => {
+	    let mut v = expand_rule(rules, a);
+	    v.append(&mut expand_rule(rules, b));
+	    v
+	}
+	Rule::TwoOr((a, b), (c, d)) => {
+	    let mut v = vec!["(".to_string()];
+	    v.append(&mut expand_rule(rules, a));
+	    v.append(&mut expand_rule(rules, b));
+	    v.push("|".to_string());
+	    v.append(&mut expand_rule(rules, c));
+	    v.append(&mut expand_rule(rules, d));
+	    v.push(")".to_string());
+	    v
+	}
+	Rule::Four(a, b, c, d) => {
+	    let mut v = expand_rule(rules, a);
+	    v.append(&mut expand_rule(rules, b));
+	    v.append(&mut expand_rule(rules, c));
+	    v.append(&mut expand_rule(rules, d));
+	    v
+	}
+    }
+}
+
 fn part1(input: &Parsed) -> Answer {
     let (rules, strings) = input;
-    println!("{:?}", rules);
-    0
+    // Expand the rules
+    let expanded = expand_rule(rules, 0).join("");
+    let re = Regex::new(&expanded).unwrap();
+    let mut n = 0;
+    for s in strings {
+	if let Some(mat) = re.find(s) {
+	    if mat.start() == 0 && mat.end() == s.len() {
+		n += 1;
+	    }
+	}
+    }
+    n
 }
 
 fn part2(_: &Parsed) -> Answer {
